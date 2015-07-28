f2186645d8fe04271b3ad59c037ebe2f789e739a
Witold Antkiewicz <vitekant@gmail.com>
2015-07-28 08:11:55 -0700

    Extended OptimizationEngine for 2 targets query.

--- CsGoTrader/OptimizationEngine.cs
+++ CsGoTrader/OptimizationEngine.cs
@@ -25,31 +25,43 @@ namespace CsGoTrader
             Decision x2 = new Decision(Domain.IntegerRange(0, 10), "x2");
             Decision x3 = new Decision(Domain.IntegerRange(0, 10), "x3");
 
-            Decision fn = new Decision(Domain.IntegerRange(0, 1), "fn");
-            Decision mw = new Decision(Domain.IntegerRange(0, 1), "mw");
-            Decision ft = new Decision(Domain.IntegerRange(0, 1), "ft");
+            Decision t1fn = new Decision(Domain.IntegerRange(0, 1), "t1fn");
+            Decision t1mw = new Decision(Domain.IntegerRange(0, 1), "t1mw");
+            Decision t1ft = new Decision(Domain.IntegerRange(0, 1), "t1ft");
+
+            Decision t2fn = new Decision(Domain.IntegerRange(0, 1), "t2fn");
+            Decision t2mw = new Decision(Domain.IntegerRange(0, 1), "t2mw");
+            Decision t2ft = new Decision(Domain.IntegerRange(0, 1), "t2ft");
 
 
 
             Rational M = 100;
 
             //model.AddDecisions(x1, x2);
-            model.AddDecisions(x1, x2, x3, fn, mw, ft);
+            model.AddDecisions(x1, x2, x3, t1fn, t1mw, t1ft, t2fn, t2mw, t2ft);
 
             model.AddConstraint("Row0", x1 + x2 + x3 == 10);
-            model.AddConstraint("Quality", fn + mw + ft == 1);
+            model.AddConstraint("Quality1", t1fn + t1mw + t1ft == 1);
+            model.AddConstraint("Quality2", t2fn + t2mw + t2ft == 1);
 
             //string fnTerm = ""
             //model.AddConstraint("Row1", (x1 * 0.035 + x2 * 0.11) / 10 <= 0.07);
-            model.AddConstraint("Row1", " fn - 1 + ((x1 * 0.035 + x2 * 0.11 + x3 * 0.25) / 10) <= 0.07");
-            model.AddConstraint("Row2", " mw - 1 + ((x1 * 0.035 + x2 * 0.11 + x3 * 0.25) / 10) <= 0.15");
-            model.AddConstraint("Row3", " ft - 1 + ((x1 * 0.035 + x2 * 0.11 + x3 * 0.25) / 10) <= 0.35");
+
+            //target 1
+            model.AddConstraint("Target1FN", " t1fn - 1 + ((x1 * 0.035 + x2 * 0.11 + x3 * 0.25) / 10) <= 0.07");
+            model.AddConstraint("Target1MW", " t1mw - 1 + ((x1 * 0.035 + x2 * 0.11 + x3 * 0.25) / 10) <= 0.15");
+            model.AddConstraint("Target1FT", " t1ft - 1 + ((x1 * 0.035 + x2 * 0.11 + x3 * 0.25) / 10) <= 0.35");
+
+            //target 2
+            model.AddConstraint("Target2FN", " t2fn - 1 + ((x1 * 0.035 + x2 * 0.11 + x3 * 0.25) / 10) * 0.72 + 0.06 <= 0.07");
+            model.AddConstraint("Target2MW", " t2mw - 1 + ((x1 * 0.035 + x2 * 0.11 + x3 * 0.25) / 10) * 0.72 + 0.06 <= 0.15");
+            model.AddConstraint("Target2FT", " t2ft - 1 + ((x1 * 0.035 + x2 * 0.11 + x3 * 0.25) / 10) * 0.72 + 0.06 <= 0.35");
 
             //model.AddConstraint("Row2", mw * (x1 * 0.035 + x2 * 0.11) / 10 <= 0.15);
 
             //model.AddConstraint("Row2", x2 + z * 100 <= 100);
 
-            Goal goal = model.AddGoal("Goal0", GoalKind.Maximize, fn * 76 + mw * 61 + ft * 34 - (x1 * 9 + x2 * 6 + x3 * 5));
+            Goal goal = model.AddGoal("Goal0", GoalKind.Maximize, (t1fn * 120 + t1mw * 61 + t1ft * 34 + t2fn * 76 + t2mw * 61 + t2ft * 34) - (x1 * 9 + x2 * 8 + x3 * 5)*2);
 
             //Goal goal = model.AddGoal("Goal0", GoalKind.Maximize,
             //    "Max[x1-0.07,0] * 4 - x1");
@@ -61,7 +73,7 @@ namespace CsGoTrader
             Console.WriteLine("x1: {0}, x: {1}", x1, x2, x3);
             Console.WriteLine("Gain: {0}", goal);
 
-            //Console.Write("{0}", report);
+            Console.Write("{0}", report);
             context.ClearModel();
         }
     }
