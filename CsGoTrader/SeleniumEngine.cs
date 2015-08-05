0ab8cc3db1513d269ef660e3662583f0e073c68c
Witold Antkiewicz <vitekant@gmail.com>
2015-08-04 21:07:13 -0700

    Multiple buy requests in trader.

--- CsGoTrader/SeleniumEngine.cs
+++ CsGoTrader/SeleniumEngine.cs
@@ -41,6 +41,32 @@ namespace CsGoTrader
             driver.Dispose();
         }
 
+        public void buySkins(List<Tuple<Skin, Quality, double, int>> skinsToBuy)
+        {
+            Dictionary<Tuple<Skin, Quality, double, int>, int> skinsBought = new Dictionary<Tuple<Skin, Quality, double, int>, int>();
+            foreach(var order in skinsToBuy)
+            {
+                skinsBought.Add(order, 0);
+            }
+
+            while(skinsToBuy.Count != 0)
+            {
+                foreach(var order in skinsToBuy)
+                {
+                    var url = getMarketUrl(order.Item1.name, order.Item2);
+                    if (buyAttempt(url, order.Item3))
+                    {
+                        skinsBought[order]++;
+                    }
+
+                    Thread.Sleep(5000);
+                }
+
+                skinsToBuy = skinsToBuy.Where(order => order.Item4 > skinsBought[order]).ToList();
+                Thread.Sleep(15000);
+            }
+        }
+
         public void buySkin(Skin skin, Quality quality, double priceLimit, int skinsToBuy)
         {
 
@@ -171,6 +197,87 @@ namespace CsGoTrader
                     @"http://steamcommunity.com/market/listings/730/{0}%20%28{1}%29",
                     skinName,
                     EnumUtil.qualityToString(quality));
+        }
+
+        public bool buyAttempt(string url, double priceLimit)
+        {
+            driver.Navigate().GoToUrl(url);
+            if (!IsElementPresent(By.Id("searchResultsRows"))){
+                Thread.Sleep(15000);
+                return false;
+            }
+
+            var results = driver.FindElementById("searchResultsRows");
+            var listings = driver.FindElementsByClassName("market_recent_listing_row");
+            double price = -1;
+            foreach (var listing in listings)
+            {
+                var priceElement = listing.FindElement(By.ClassName("market_listing_price_with_fee"));
+                var priceString = priceElement.Text;
+                priceString = Regex.Replace(priceString, "[^0-9.,]", "");
+
+                if (string.IsNullOrWhiteSpace(priceString))
+                {
+                    continue;
+                }
+
+                if (double.TryParse(priceString, out price))
+                {
+                    break;
+                }
+            }
+
+            if (!(price > -1))
+            {
+                return false;
+            }
+
+            try
+            {
+                if (price <= priceLimit)
+                {
+                    var button = listings.First().FindElement(By.XPath("//a[contains(@class,'item_market_action_button_green')]"));
+                    button.Click();
+                    if (IsElementPresent(By.Id("market_buynow_dialog_accept_ssa")))
+                    {
+                        var checkBox = driver.FindElementById("market_buynow_dialog_accept_ssa");
+                        if (checkBox.Selected != true)
+                        {
+                            checkBox.Click();
+                        }
+
+                        var finalButton = driver.FindElementById("market_buynow_dialog_purchase");
+                        finalButton.Click();
+
+                        if (IsElementPresentWithRetry(By.Id("market_buynow_dialog_purchasecomplete_message"), 15))
+                        {
+                            Console.WriteLine("{0}:Item purchased for: {1}", DateTime.Now, price);
+                            return true;
+                        }
+                        else
+                        {
+                            //check visibility div id = market_buynow_dialog_error
+                            Console.WriteLine("Purchase failed");
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("{0}:Item probably purchased without rules confirmation for: {1}", DateTime.Now, price);
+
+                        return true;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine(string.Format("{0}: Available price: {1} is higher that required price: {2}", DateTime.Now, price, priceLimit));
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(string.Format("{1}: Exception encountered {0}", ex, DateTime.Now));
+            }
+
+            return false;
         }
     }
 }
--- CsGoTrader/Trader.cs
+++ CsGoTrader/Trader.cs
@@ -12,7 +12,15 @@ namespace CsGoTrader
         {
             var selenium = new SeleniumEngine();
             selenium.initializeWebDriver();
-            selenium.buySkin(new Skin("UMP-45 | Scorched", 0, 1, CollectionGrade.Classified), Quality.WellWorn, 0.4, 4);
+            selenium.buySkins(new List<Tuple<Skin, Quality, double, int>>()
+            {
+                //new Tuple<Skin, Quality, double, int>(new Skin("CZ75-Auto | Pole Position", 0, 1, CollectionGrade.Classified), Quality.FactoryNew, 0.75, 10),
+                new Tuple<Skin, Quality, double, int>(new Skin("SG 553 | Cyrex", 0, 1, CollectionGrade.Classified), Quality.FactoryNew, 7.5, 5),
+                new Tuple<Skin, Quality, double, int>(new Skin("CZ75-Auto | Yellow Jacket", 0, 1, CollectionGrade.Classified), Quality.FactoryNew, 7.5, 5),
+                new Tuple<Skin, Quality, double, int>(new Skin("MP7 | Nemesis", 0, 1, CollectionGrade.Classified), Quality.FactoryNew, 7.5, 5),
+                new Tuple<Skin, Quality, double, int>(new Skin("MP9 | Ruby Poison Dart", 0, 1, CollectionGrade.Classified), Quality.FactoryNew, 0.75, 10)
+            });
+            //selenium.buySkin(new Skin("CZ75-Auto | Pole Position", 0, 1, CollectionGrade.Classified), Quality.FactoryNew, 0.75, 10);
             selenium.deinitializeDriver();
         }
     }
